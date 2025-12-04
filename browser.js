d50bce73ff0f8733554e048c9bfd0747d769b4a5
Alex Wine <alexwine36@gmail.com>
2025-12-04 10:19:06 -0700

    rename to personal scope

--- browser.js
+++ browser.js
@@ -1,1 +1,1 @@
-export * from '@napi-rs/package-template-pnpm-wasm32-wasi'
+export * from '@napi-rs/takeoff-wasm32-wasi'
--- simple-test.js
+++ simple-test.js
@@ -1,5 +1,7 @@
-const { plus100 } = require('./index')
+const { plus100, sum } = require('./index')
 
 console.assert(plus100(0) === 100, 'Simple test failed')
+
+console.log('From native', sum(40, 2))
 
 console.info('Simple test passed')
